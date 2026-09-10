3797fdaba7c9e3a16ae579a9832eed2ec887c7ab
ZhangWeinian <zhangweinian.cqupt@gmail.com>
2026-09-10 16:57:54 +0800

    添加 cy_raspberry_pi 定制版官方 demo (arm64 板端适配)
    - CMakeLists: project(C CXX) + SYSTEM_ARCH_LINUX, 修复官方 C++ 版无法编译的问题
    - 填入 PSDK 用户信息 (从 cy_psdk 同步)
    - 串口对齐板端设备 /dev/ttyUSB0, chmod 静音避免刷屏
    - .gitignore: 新增 build-arm64/ 构建产物忽略

---
+++ samples/sample_c++/platform/linux/cy_raspberry_pi/application/dji_sdk_app_info.h
@@ -0,0 +1,60 @@
+/**
+ ********************************************************************
+ * @file    dji_sdk_app_info.h
+ * @brief   This is the header file for defining the structure and (exported) function prototypes.
+ *
+ * @copyright (c) 2018 DJI. All rights reserved.
+ *
+ * All information contained herein is, and remains, the property of DJI.
+ * The intellectual and technical concepts contained herein are proprietary
+ * to DJI and may be covered by U.S. and foreign patents, patents in process,
+ * and protected by trade secret or copyright law.  Dissemination of this
+ * information, including but not limited to data and other proprietary
+ * material(s) incorporated within the information, in any form, is strictly
+ * prohibited without the express written consent of DJI.
+ *
+ * If you receive this source code without DJI’s authorization, you may not
+ * further disseminate the information, and you must immediately remove the
+ * source code and notify DJI of its removal. DJI reserves the right to pursue
+ * legal actions against you for any loss(es) or damage(s) caused by your
+ * failure to do so.
+ *
+ *********************************************************************
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef DJI_SDK_APP_INFO_H
+#define DJI_SDK_APP_INFO_H
+
+/* Includes ------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Exported constants --------------------------------------------------------*/
+// ATTENTION: User must goto https://developer.dji.com/user/apps/#all to create your own dji sdk application, get dji sdk application
+// information then fill in the application information here.
+#define USER_APP_NAME               "cy_psdk"
+#define USER_APP_ID                 "175454"
+#define USER_APP_KEY                "06775bab4c72160a4a5cb2a34403025"
+#define USER_APP_LICENSE                                                              \
+	"vyT4kLSd5hn9CTv0yG9chZTkyzgN4m4LeUfEJNiiHJFdFsjWCdf3GJG6CcAaRPWajYYOfvzBQJLaVF6" \
+	"dWd1VG6nXshLmXntkjpNbq0TEXf82SZJoY41Qa2OAv5CxG++uVpYSB7jfPnaLY9zddzeC/w3HieT"    \
+	"qBybsVokOCIzj7PKE9b4H6m3YpZWkRpOEvER/yFJ2ibRwFghv4+XV0TP8p/PVuGtvOuE3fq7SmSH"    \
+	"R60y4bdgpXj1S1Xv+UVdz75KPqTvUPD2AYWlHDsi+3TUPPRhZHF+nuubq/9pSdVnGaFwm7RM/L"      \
+	"d8OPNAs54Ypy7YmqRxvpb8qzBSA8CMHLMqVRA=="
+#define USER_DEVELOPER_ACCOUNT      "accepted"
+#define USER_BAUD_RATE              "460800"
+
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported functions --------------------------------------------------------*/
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // DJI_SDK_APP_INFO_H
+/************************ (C) COPYRIGHT DJI Innovations *******END OF FILE******/
